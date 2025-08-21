bbd957b253aee5a1fbe9fc319fcc443df7f8284d
Sim <simran.dhaliwal@graze.com>
2025-08-21 21:19:31 +0100

    made local env fixes

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -18,13 +18,23 @@ export default function HeroSection() {
       
       {/* Bottom Section with Logo on Green Background - Like Billy's */}
       <div className="relative z-10 bg-lime py-8 wavy-cloud-top">
-        <div className="container mx-auto px-4 text-center">
-          <img 
-            src={auntieLogoUrl} 
-            alt="Auntie's Logo" 
-            className="mx-auto w-24 md:w-32 lg:w-40 h-auto"
-            data-testid="img-auntie-logo-bottom"
-          />
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-center">
+            <div className="text-right text-black font-bold text-2xl md:text-3xl lg:text-4xl" style={{ flexBasis: '40%' }}>
+             ALL-NATURAL INGREDIENTS
+            </div>
+            <div className="flex-shrink-0 mx-8">
+              <img 
+                src={auntieLogoUrl} 
+                alt="Auntie's Logo" 
+                className="w-24 md:w-32 lg:w-40 h-auto"
+                data-testid="img-auntie-logo-bottom"
+              />
+            </div>
+            <div className="flex-1 text-left text-black font-bold text-2xl md:text-3xl lg:text-4xl">
+              INDIAN INSPIRED
+            </div>
+          </div>
         </div>
       </div>
     </section>
